8a7f12b2924b5d136172cd0e3147050767c141a0
simmeringstew <dev.95a2e@simplelogin.com>
2023-01-17 15:54:32 -0500

    added the animal template to index

--- src/index.js
+++ src/index.js
@@ -3,5 +3,67 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+const animals = [
+    {
+        clicked: false,
+        src: "./images/cheetah.jpeg",
+        id: 1
+    },
+    {
+        clicked: false,
+        src: "./images/crocodile.jpeg",
+        id: 2
+    },
+    {
+        clicked: false,
+        src: "./images/elephant.jpeg",
+        id: 3
+    },
+    {
+        clicked: false,
+        src: "./images/giraffe.jpeg",
+        id: 4
+    },
+    {
+        clicked: false,
+        src: "./images/gorilla.jpeg",
+        id: 5
+    },
+    {
+        clicked: false,
+        src: "./images/hippopotamus.jpeg",
+        id: 6
+    },
+    {
+        clicked: false,
+        src: "./images/hyena.jpeg",
+        id: 7
+    },
+    {
+        clicked: false,
+        src: "./images/lion.jpeg",
+        id: 8
+    },
+    {
+        clicked: false,
+        src: "./images/ostrich.jpeg",
+        id: 9
+    },
+    {
+        clicked: false,
+        src: "./images/rhino.jpeg",
+        id: 10
+    },
+    {
+        clicked: false,
+        src: "./images/wildebeest.jpeg",
+        id: 11
+    },
+    {
+        clicked: false,
+        src: "./images/zebra.jpeg",
+        id: 12
+    }
+];
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
